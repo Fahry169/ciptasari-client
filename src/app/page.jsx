29ebab5b9be6ce5aa5f6c96c8a91552fd3ac38c4
fahry firdaus <ffahry169@gmail.com>
2025-06-28 17:55:41 +0700

    config: installation heroui

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "@heroui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div>
+  <Button>
+    Hello
+  </Button>
+ </div>
+)
+}
---
+++ src/app/providers.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as React from "react";
+
+import { HeroUIProvider } from "@heroui/react";
+
+export default function Providers({ children }) {
+  return <HeroUIProvider>{children}</HeroUIProvider>;
+}
